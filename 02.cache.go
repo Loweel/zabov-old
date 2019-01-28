b294aa14d8ba8106344fe869733ade7f019b88c6
Loweel <loweel@gmx.de>
2019-01-28 17:39:48 +0100

    Typo

--- 02.cache.go
+++ 02.cache.go
@@ -64,7 +64,7 @@ func GetDomainFromCache(s string) *dns.Msg {
 	}
 
 	conf, errDB = MyZabovCDB.Get([]byte(s), nil)
-	if errDB != null {
+	if errDB != nil {
 		fmt.Println("Cant READ DB :" , errDB.Error() )
 		return nil
 	}
